9fddd1f4b3bce4c65e78d89e8a378849a2a7bb52
Stefan Pejcic <pejcha1994@gmail.com>
2020-02-12 11:02:11 +0100

    Create every-5min.bat

---
+++ every-5min.bat
@@ -0,0 +1,34 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Dozvole
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> Ako je error flag, nije admin
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+
+:loop
+echo Clearing...
+EmptyStandbyList.exe standbylist
+timeout 300
+cls
+goto loop
